be47704fd48cf268f26d48d73925b887c344a38b
TentativeConvert <marcus.zibrowius@cantab.net>
2016-12-18 14:48:21 +0100

    initial commit

---
+++ Latex_Biport.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# For some reason, import fails if the folder ("latexbiport") 
+# has the same name as this file ("Latex_Biport").
+import latexbiport.lateximport
+import latexbiport.latexexport
