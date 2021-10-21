c4adf52cb1dc5686386f189e28b7f0c67b045804
Marcus Zibrowius <marcus.zibrowius@cantab.net>
2021-10-21 17:31:23 +0200

    modified:   make_zip_files_for_upload.sh

--- make_zip_files_for_upload.sh
+++ make_zip_files_for_upload.sh
@@ -3,9 +3,15 @@ zip -r anki20.zip .
 cd ..
 mv ./Anki\ 2.0/anki20.zip ./
 
-cd Anki\ 2.1/latexbiport
-zip -r anki21.zip .
+cd Anki\ 2.1.0-2.1.44/latexbiport
+zip -r anki21a.zip .
 cd ..
 cd ..
-mv ./Anki\ 2.1/latexbiport/anki21.zip ./
+mv ./Anki\ 2.1.0-2.1.44/latexbiport/anki21a.zip ./
+
+cd Anki\ 2.1.45-/latexbiport
+zip -r anki21b.zip .
+cd ..
+cd ..
+mv ./Anki\ 2.1.45-/latexbiport/anki21b.zip ./
 
